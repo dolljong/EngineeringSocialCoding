b8994f1f193977f2a637ef3f76a7e49716343a2d
sukjong <dolljong@gmail.com>
2023-02-12 15:43:04 +0900

    Revert "Seolxlwings"

--- seol/secxlwings/test.py
+++
@@ -1,8 +0,0 @@
-datadic = {'fck' : 30, 'fy' : 400}
-datadic1 = {'pic' : 0.65, 'pis' : 0.7}
-fck = datadic['fck']
-fy = datadic['fy']
-pic = datadic1['pic']
-pis = datadic1['pis']
-print(f"fck:{fck} fy:{fy} pic:{pic} pis:{pis}")
-
